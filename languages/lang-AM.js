8a99b448b23d83221949807879fe6613c21ab842
Ouamassi <tosuccesspractice@gmail.com>
2025-02-20 17:16:41 +0100

    first

---
+++ languages/lang-AM.js
@@ -0,0 +1,128 @@
+﻿const JS_eLang =
+{
+	
+	HereMosqueName		: "የመስጂዱ ስም",
+	DohaTime			: "ዱሃ",
+	MinutesToIqama		: "ኢቃማ ...",
+	MinutesToDoha		: "ዱሃ ...",
+	AzanCalling			: "አዛን",
+	AzanNAME			: "አዛን",
+	NextPrayTime	 	: "ለቀጣዩ አዛን የቀረው",
+	NamesOfPrayings		: "ፈጅር,ሹሩቅ,ዙሁር,ዓስር,መግሪብ,ዒሻእ",
+	NamesWeekDays		: "እሁድ,ሰኞ,ማክሰኞ,ረቡዕ,ሀሙስ,ጁሙዓ,ቅዳሜ",
+	NamesMonthsMiladi	: "ጃኑዋሪ, ፌብሩዋሪ, ማርች, አፕሪል, ሜይ, ጁን, ጁላይ, ኦገስት, ሴፕቴምበር, ኦክቶበር , ኖቬምበር, ዲሴምበር",
+    MenuMonthsHijri		: "ሙሐረም, ሰፍፈር, ረቢዑል አወል, ረቢዑ ሣኒ, ጁማደል አወል, ጁማደ ሣኒ, ረጀብ, ሻዕባን, ረመዷን, ሸዋል, ዙል ቃዒዳ, ዙል ሂጃ",
+	
+	MenuFullScreen		: "ሙሉ ስክሪን",
+	MenuMosqueNAME		: "የመስጂዱን ስም ቀይር",
+	MenuMosqueMessage 	: "የመስጂዱ ማስታወቂያ",
+	MenuLocation		: "የመስጂዱ መገኛ",
+	MenuHijriClick		: "የሒጅሪ ቀን ማስተካከያ",
+	MenuThemes			: "ባክግራውንድ ቀይር",
+	MenuOptions			: "አማራጮች",
+	MenuAzkar			: "አዝካር ማሳያ",
+	MenuSliders			: "ተንሸራታች ማሳያ",
+	MenuAdjustments		: "የሰላት እና የኢቃማ ሰአት አስተካክል",
+	MenuBlackscreen		: "ጥቁር ስክሪንን አስተካክል",
+	MenuAboutTawkit		: "ስለ እኛ",
+
+	XXDoaAzanIqama		: "በአዛን እና በኢቃም መካከል የሚደረግ ዱዓ ተመላሽ አይሆንም!",
+	XXFastingMonday		: "ነገ ሰኞ የመጾምን ሱና እንዳትረሳ!",
+	XXFastingThursday	: "ነገ ኸሚስ የመጾምን ሱና እንዳትረሳ!",
+	
+	XX_OPTION_0			: "በ24 ሰአት አስቆጥር",
+	XX_OPTION_1			: "ሙሉ ሰአት አድርግ",
+	XX_OPTION_2			: "ያለፉ ሰላቶችን አደብዝዝ",
+
+	XX_ADJUST_1			: "Countdown ቆጣሪውን አስተልቅ",
+	XX_ADJUST_2			: "ለአዛን ድምፅ ተጠቀም (MP3)",
+	XX_ADJUST_3			: "በረመዳን ለዒሻእ ሶላት 30 ደቂቃ ጨምር",
+	
+	XX_PRAYER			: "ሶላት",
+	XX_IQAMA			: "ኢቃማ",
+	XX_DOHA				: "አድ ዱሃ",
+	
+	
+	XX_DIMMER_0			: "በሰላት ወቅት እስክሪኑን አጥቁር",
+	XX_DIMMER_1			: "ፀሀይ መውጫ ሰአት ላይ ለ30 ደቂቃ እስክሪኑን አጥቁር",
+	XX_DIMMER_2			: "ከዒሻእ ሶላት በኋላ ለ60 ደቂቃ እስክሪኑን አጥቁር",
+	XX_DIMMER_3			: "ከዙሁር በፊት እስክሪኑን አጥቁር",
+	XX_DIMMER_4			: "ከዙሁር በኋላ እስክሪኑን አሳይ",
+	XX_DIMMER_5			: "Prayers Durations To Hide Screen",
+	
+	
+	XX_TIMES_0			: "Use Calculation",
+	XX_TIMES_1			: "Use Files",
+	XX_TIMES_2			: "የመስጂድህን መገኛ ሃገር ምረጥ",
+	XX_TIMES_3			: "ሃገርህ በዝርዝሩ ውስጥ ካልተካተተ, select (00.000.PERSONAL)",
+	XX_TIMES_4			: "ይህን ፋይል ቀያይረህ (/data/00/wtimes-00.000.js) ካንተ መስጂድ ጋር እንዲስተካከል አድርግ ",
+	XX_TIMES_5			: "አፕሊኬሽኑን እንደገና አስጀምር",
+	
+	XX_TIMES_6			: "ቀያይር",
+	
+	
+	XX_BOX_0			: "የሂጅሪ ቀን ቆጣሪውን ለማስተካከል የሂጅሪ ቆጣሪውን ይነካኩት + 2 / -2",
+	XX_MISC_0			: "ለውጥ ካደረክ ቡሃላ ፔጁን እንደገና አስጀምር !",
+	XX_MISC_1			: "ፈጅር የሚለውን እና ስም/ሰዓት የሚለውን በመንካት theme ይቀያይሩ",
+	
+	XX_NO_SELL			: "የነፃ አፕሊኬሽን ነው! መሸጥ ክልክል ነው!",
+	
+	XX_JOMOA			: "ጁሙዓ",
+	XX_IQAMAASHOUR		: "ኢቃማውን እንደ ሰአት አሳይ",
+	XX_SUMMERTIME		: "ክረምት ሲሆን የሰላት ወቅቶቹ ላይ 1 ሰአት ጨምር",
+	
+	W9_ARABICDIGITS		: "የአረበኛ ቁጥሮችን አሳይ",
+	XX_WCSV_MESSAGE		: "ለሰላትና ኢቃማ ወቅቶች የራስህን የግዜ ሰንጠረዥ መጠቀሚያ አብራ.<br>( JAMAAT TIMES ) (wcsv.js) የተሰኘውን ፋይል ከ app ፎልደር ቀያይር.",
+	W9_eCOUNTDOWNOnOff	: "Countdown ቆጣሪውን አሳይ",
+	XX_1MOREHOUR		: "Manually 1 ሰአት በሁሉም የሰላት ወቅቶች ላይ ጨምር",
+	XX_1LESSHOUR		: "Manually 1 ሰአት ከሁሉም የሰላት ወቅቶች ላይ ቀንስ",
+	
+	XX_eShortAZAN		: "አጭር የአዛን ድምፅ ተጠቀም",
+	XX_eShortIQAMA		: "አጭር የኢቃማ ድምፅ ተጠቀም",
+	XX_YearlyWTIMES		: "አመታዊ የወቅቶች data file ለማዘጋጀት , ወደዚህ ሂድ:",
+	XX_METEO			: "የአየር ንብረት ትንበያ",
+	APP_FONTS			: "Application Fonts",
+	W9_AYATS_ACTIVE		: "የቁመት ስክሪን ላይ አንቀፆችንና ሃዲሶችን አሳይ",
+
+	XX_FORCED_VERTICAL	: "የቁመት ስክሪን",
+	XX_SCROLLABLE_MSG	: "ማስታወቆያዎችን auto-scrollable አድርግ",
+	XX_HIDE_IQAMAT		: "(Home Clock) የኢቃማ ሰአቱን ደብቅ",
+	XX_HIDEPROGRESS		: "Progress Circleሉን ደብቅ",
+	XX_ZEROS_AMPM		: "በ AM/PM አቆጣጠር ላይ ዜሮ ጨምር፤ ምሳሌ(04:35 AM)",
+	XX_METEOPRYRS		: "ከሰላት ወቅቶቹ አጠገብ የአየር ንብረት ማሳያ ጨምር",
+	XX_METEO_A			: "የአየር ንብረት ትንበያ",
+	XX_TIME2SHRQ		: "ሹሩቅ...",
+	XX_WQT_SHRQ			: "ሹሩቅ",
+	XX_5BOXES_ONLY		: "የአግድም እስክሪን ላይ የሰላት እና ኢቃማ ማሳያ ፎንት በትላልቁ ይሁን",
+	XX_IQAMAT_SALAT		: "ለሰላት ተቆመ !",
+	XX_SHOWNAMES		: "የቁመት እስክሪን ላይ ርእሶችን አሳይ",
+	
+	XX_JOMOA_ONOFF		: "የጁሙዓ ኹጥባ መጀመሪያ ሰአት",
+	XX_MSQ_ELECTRICITY	: "የመስጂዱ ኢለክትሪክ ማኔጅ ማድረጊያ",
+	XX_JANAZAT_COUNTER	: "በአግድም እስክሪን ግዜ የጀናዛ ቆጣሪውን አሳይ",
+	XX_COMMANDER_AUDIO	: "Activate Commands Scheduler ( Voice or HTTP )",
+	XX_COMMANDER_HTTP	: "http_commander (not yet)",
+	XX_JANAZAT_TEXTER	: "ይህ አማራጭ የጀናዛዎችን ብዛት በሰላት ወቅቶች ላይ ያሳያል። የጀናዛ ቁጥር ለመጨመር የጥቁሩን ክብ መሃል ነካካ። ለመቀነስ ደግሞ የጥቁሩን ክብ ከታቹ ነካካ",
+	XX_ADMINMSGS_0		: "የመስጂዱ ማስታወቂያዎችን በስክሪኑ ታች",
+	XX_ADMINMSGS_TEXTER	: "እዚህ የቁርአን አንቀፆችን ሃዲሶችን እና ጠቃሚ መልክቶችን ከእስክሪኑ ታች እንዲታዩ ማድረግ ትችላለህ። መልክቱ የሚሽከረከር ወይም የቆመ ሆኖ የሚቀያየር እንዲሆንም መምረጥ ትችላለህ።",
+	XX_ADMINMSGS_HELP	: "መረጃ: መልክት ለመጨመር መልክት ጨምር የሚለውን ሁለቴ ነካካ.<br> ቀኑን ወይም የመልክቱን ቀን ለመቀየር እለታዊ የሚለውን ሁለቴ ነካካ",
+
+	XX_ADD_MSG			: "አዲስ መልእክት ጨምር",
+	XX_DOHR_XMIN_ASR	: "Dohr = Asr - X min. (Click to change) : ",
+	XX_JOMOA_INLOGO		: "በአግድም እስክሪን ግዜ የጁሙዓ ሰአት በሎጎው ቦታ ይታይ",
+	MenuDimmerDirect	: "እስክሪኑን Manualy ደብቅ",
+	XX_AZKAR_0			: "አዝካሮችን ከሰላት በኋላ እንዲታይ አድርግ",
+	XX_SLIDERS_0		: "እስላይዶችን አብራ",
+	XX_JOMOA_SHOW		: "የጁሙዓን ሰአት ስክሪን ላይ አሳይ ",
+	XX_VR_MIDDLENAMES	: "በቁም እስክሪን ግዜ የሶላቱ ስም መሃል ላይ ይሁን",
+	XX_VR_NoNEXTNoJMA	: "በቁም እስክሪን ግዜ ጁሙዓ እና ቀጣይ የሰላት ሰአቶችን አጥፍቶ የሰአት ቆጣሪው ትላልቅ እንዲሆን አድርግ",
+	MenuAzkarSabahMasaa	: "የጧትና ማታ ውዳሴዎች",
+	XX_AZKAR_SABAH		: "የጧት ውዳሴን አስጀምር, ከፈጅር ቡኋላ",
+	XX_AZKAR_ASR		: "የማታ ውዳሴን አስጀምር, ከዓስር ቡኋላ",
+	XX_AZKAR_MAGRIB		: "የማታ ውዳሴን አስጀምር, ከመግሪብ ቡኋላ",
+	XX_AZKAR_ISHA		: "የማታ ውዳሴን አስጀምር, ከዒሻእ ቡኋላ",
+	XX_1MIN_COUNTER		: "የመጨረሻ ደቂቃ ግዜ ቆጣሪውን በትልቁ አሳይ",
+	XX_IQAMASCREEN		: "እስክሪኑ ከመጥቆሩ በፊት የኢቃማ እስክሪን አሳይ",
+
+
+};
